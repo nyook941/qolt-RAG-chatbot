2ca2be234344c198f6ac0e5042681666b51faea9
nyook941 <kennan.k.wu@gmail.com>
2023-10-24 17:03:37 -0500

    add in websocket to front end

--- ChatbotFE/src/App.js
+++ ChatbotFE/src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Sockette from "sockette";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+  let ws = null; // Initialize WebSocket outside of useEffect
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const sendMessage = () => {
+    if (ws) {
+      const payload = {
+        action: "startTranscription",
+        message: "test.mp3",
+      };
+      ws.json(payload);
+      console.log("Sent:", payload);
+    }
+  };
+
+  useEffect(() => {
+    ws = new Sockette(
+      "wss://ehj9s7fnwb.execute-api.us-east-2.amazonaws.com/production",
+      {
+        onmessage: (e) => {
+          try {
+            console.log("Received:", JSON.parse(e.data));
+          } catch (error) {
+            console.log("Error parsing message:", e.data, error);
+          }
+        },
+      }
+    );
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={sendMessage}>Send Message</button>
       </header>
     </div>
   );
